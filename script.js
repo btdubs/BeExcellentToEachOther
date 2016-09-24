d86821d966a7c75e416831ad33386d4b1aa2e52e
Brian <brianco1998@gmail.com>
2016-09-24 08:11:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var api = "http://api.giphy.com/v1/gifs/search?";
+var apiKey = "&api_key=dc6zaTOxFJmzC";
+var query = "&q=";
+
+var queryList = ["dog","bill+ted+excellent","high+five","dog+high+five","excellent","thumbs+up"]
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getQuery(){
+	var index = getRandomInt(0,queryList.length)
+	return queryList[index]
+}
+
+
+
+function getSetGif() {
+  var url = api + apiKey + query + getQuery();
+  $.getJSON(url, function gotData(data){
+    randomIndex = getRandomInt(0,data.data.length)
+    var gifUrl = data.data[randomIndex].images.original.url
+    $("body").css('background-image', 'url(' +gifUrl+ ')')
+  });
+}
+
+$(document).ready(function(){
+    $("#btn").click(function(){
+      $("#btn").css('display','none')
+      getSetGif()
+      $("#btn2").css('display','block')
+    });
+    $("#btn2").click(function(){
+      getSetGif()
+    });
+});
